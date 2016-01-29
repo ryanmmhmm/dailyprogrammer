f4bb105ac75f00fd95111513df4680b88f6eccf6
ryanmmhmm <magowan.ryan@gmail.com>
2016-01-29 11:38:53 -0500

    numbers parsing out as array of number characters

--- 250/250_sdn.rb
+++ 250/250_sdn.rb
@@ -1,10 +1,21 @@
 
 class SelfDescriptiveNumber
 
-  attr_reader :number
+  attr_reader :number, :individual_numbers
 
   def initialize(number)
     @number = number
+    @individual_numbers = split_numbers_into_array
+  end
+
+  def split_numbers_into_array
+    if @number.is_a?(Integer)
+      @individual_numbers = @number.to_s
+    else
+      @individual_numbers = @number
+    end
+
+    @individual_numbers.split('')
   end
 
 end
--- 250/test_250_sdn.rb
+++ 250/test_250_sdn.rb
@@ -4,11 +4,15 @@ require '250_sdn.rb'
 class TestSDN < Minitest::Test
 
   def setup
-    @input = SelfDescriptiveNumber.new("120")
+    @input = SelfDescriptiveNumber.new(120)
   end
 
   def test_that_program_accepts_input
-    assert_equal("120", @input.number)
+    assert_equal(120, @input.number)
+  end
+
+  def test_that_number_split_into_array_of_numbers
+    assert_equal(["1", "2", "0"], @input.individual_numbers)
   end
 
 end
